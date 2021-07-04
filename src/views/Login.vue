8c8e460127ea38f59c3b947e3976118d12f4bc6b
tsts858325910 <763943829@qq.com>
2021-07-05 00:13:35 +0800

    index页面1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -85,7 +85,7 @@ export default {
     position: relative;
     width: 100%;
     height: 100%;
-    background-image: url('../assets/img/img.jpg');
+    background-image: url('../assets/../img/img.jpg');
     background-size: 100%;
 }
 .ms-title {
